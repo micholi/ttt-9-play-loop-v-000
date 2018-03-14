6e9eb3b296677480717aa033c8329609dc7c61cd
Michelle Olivieri <michelleo72@gmail.com>
2018-03-14 17:19:58 +0000

    automatically backed up by learn

--- lib/play.rb
+++ lib/play.rb
@@ -37,9 +37,9 @@ end
 
 # Define your play method below
 def play(board)
-  play_count = 0
-  until play_count == 9
+  count = 0
+  until count == 9
     # code here
-    play_count += 1
+    count += 1
 
 end
